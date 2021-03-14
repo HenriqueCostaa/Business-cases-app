d9189b14757093231fa9de27a082e4eb16a9d252
HenriqueCostaa <75020556+HenriqueCostaa@users.noreply.github.com>
2021-03-14 21:52:45 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,70 +1,108 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import plotly.graph_objects as go
-import pandas as pd
-import numpy as np
-
-# Dataset 'Processing'
-
-df_emissions = pd.read_csv('emission_full.csv')
-
-df_emission_0 = df_emissions.loc[df_emissions['year']==2000]
-
-# Building our Graphs (nothing new here)
-
-data_choropleth = dict(type='choropleth',
-                       locations=df_emission_0['country_name'],  #There are three ways to 'merge' your data with the data pre embedded in the map
-                       locationmode='country names',
-                       z=np.log(df_emission_0['CO2_emissions']),
-                       text=df_emission_0['country_name'],
-                       colorscale='inferno',
-                       colorbar=dict(title='CO2 Emissions log scaled')
-                      )
-
-layout_choropleth = dict(geo=dict(scope='world',  #default
-                                  projection=dict(type='orthographic'
-                                                 ),
-                                  #showland=True,   # default = True
-                                  landcolor='black',
-                                  lakecolor='white',
-                                  showocean=True,   # default = False
-                                  oceancolor='azure'
-                                 ),
-                         
-                         title=dict(text='World Choropleth Map',
-                                    x=.5 # Title relative position according to the xaxis, range (0,1)
-                                   )
-                        )
-
-fig = go.Figure(data=data_choropleth, layout=layout_choropleth)
+from datetime import datetime as dt
 
 
-
-# The App itself
+options = [{'label': 'Portugal', 'value': 'PT'},
+           {'label': 'Spain', 'value': 'SP'},
+           {'label': 'France', 'value': 'FR'}]
 
 app = dash.Dash(__name__)
 
-server = app.server
+app.layout = html.Div([
 
+    html.Label('Dropdown'),
+    dcc.Dropdown(
+        id='drop',
+        options=options,
+        value='PT'
+    ),
 
+    html.Br(),
+    html.Hr(),
 
+    html.Label('Multi-Select Dropdown'),
+    dcc.Dropdown(
+        id='multidrop',
+        options=options,
+        value=['PT', 'FR'],
+        multi=True
+    ),
 
-app.layout = html.Div(children=[
-    html.H1(children='Demand forecast protype'),
+    html.Br(),
+    html.Hr(),
 
-    html.Div(children='''
-        Example of html Container
-    '''),
+    html.Label('Text Input'),
+    dcc.Input(
+        id='input',
+        value='PT',
+        type='text'),
 
-    dcc.Graph(
-        id='example-graph',
-        figure=fig
+    html.Br(),
+    html.Hr(),
+
+    html.Label('Slider'),
+    dcc.Slider(
+        id='slider',
+        min=0,
+        max=9,
+        marks={i: 'Label {}'.format(i) for i in range(1, 6)},
+        value=5,
+        step=1
+    ),
+
+    html.Br(),
+    html.Hr(),
+
+    html.Label('Range Slider'),
+    dcc.RangeSlider(
+        id='rangeslider',
+        marks={i: 'Label {}'.format(i) for i in range(0, 9)},
+        min=0,
+        max=9,
+        value=[0, 1],
+        step=1
+    ),
+    html.Br(),
+    html.Hr(),
+
+    html.Label('Text Area'),
+    dcc.Textarea(
+        id='textarea',
+        placeholder='Enter a value...',
+        value='This is a TextArea component'
+    ),
+
+    html.Br(),
+    html.Hr(),
+
+    html.Label('Date Picker Single'),
+    dcc.DatePickerSingle(
+        id='date-picker-single',
+        date=dt(2019, 6, 12)
+    ),
+
+    html.Br(),
+    html.Hr(),
+
+    html.Label('Radio Items'),
+    dcc.RadioItems(
+        id='radio',
+        options=options,
+        value='PT'
+    ),
+
+    html.Br(),
+    html.Hr(),
+
+    html.Label('Checkboxes'),
+    dcc.Checklist(
+        id='checkbox',
+        options=options,
+        value=['PT', 'FR']
     )
 ])
-
-
-
 
 if __name__ == '__main__':
     app.run_server(debug=True)
