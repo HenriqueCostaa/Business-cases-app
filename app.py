381698031b89047d9c851d1fb91b3e6a94c6c411
HenriqueCostaa <75020556+HenriqueCostaa@users.noreply.github.com>
2021-03-14 23:04:17 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,68 +1,73 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output
 import plotly.graph_objects as go
 import pandas as pd
 import numpy as np
 
-# Dataset Processing
+# Dataset 'Processing'
 
 df_emissions = pd.read_csv('emission_full.csv')
-df = df.drop(6, errors='ignore')  # Drops the 'total sales' row
 
+df_emission_0 = df_emissions.loc[df_emissions['year']==2000]
 
+# Building our Graphs (nothing new here)
 
-options = [{'label': 'Month of July', 'value': 'July'},
-           {'label': 'Month of August', 'value': 'August'},
-           {'label': 'Month of September', 'value': 'September'}]
-
-
-
-
-@app.callback(
-    Output(component_id='example-graph', component_property='figure'),
-    [Input(component_id='drop', component_property='value')]
-)
-def callback_1(input_value):
-    data_bar = dict(type='bar',
-                    y=df[input_value],
-                    x=df['Product'],
-                    texttemplate='<b>%{y} €</b>',
-                    textposition='outside'
-                    )
-
-    layout_bar = dict(yaxis=dict(range=(0, 1500),
-                                 title='Monetary Units'
-                                 )
+data_choropleth = dict(type='choropleth',
+                       locations=df_emission_0['country_name'],  #There are three ways to 'merge' your data with the data pre embedded in the map
+                       locationmode='country names',
+                       z=np.log(df_emission_0['CO2_emissions']),
+                       text=df_emission_0['country_name'],
+                       colorscale='inferno',
+                       colorbar=dict(title='CO2 Emissions log scaled')
                       )
 
-    return go.Figure(data=data_bar, layout=layout_bar)
+layout_choropleth = dict(geo=dict(scope='world',  #default
+                                  projection=dict(type='orthographic'
+                                                 ),
+                                  #showland=True,   # default = True
+                                  landcolor='black',
+                                  lakecolor='white',
+                                  showocean=True,   # default = False
+                                  oceancolor='azure'
+                                 ),
+                         
+                         title=dict(text='World Choropleth Map',
+                                    x=.5 # Title relative position according to the xaxis, range (0,1)
+                                   )
+                        )
+
+fig = go.Figure(data=data_choropleth, layout=layout_choropleth)
+
+
+
 # The App itself
 
 app = dash.Dash(__name__)
 
 server = app.server
 
-app.layout = html.Div([
-    html.H1('Exercise 3 Data Visualization Example'),
 
-    html.Br(),
 
-    html.Label('Choose a Month:'),
-    dcc.Dropdown(
-        id='drop',
-        options=options,
-        value='July'
-    ),
+
+app.layout = html.Div(children=[
+    html.H1(children='My First DashBoard'),
+
+    html.Div(children='''
+        Example of html Container
+    '''),
 
     dcc.Graph(
-        id='example-graph'
+        id='example-graph',
+        figure=fig
     )
-
 ])
 
 
+
+
 if __name__ == '__main__':
+    app.run_server(debug=True)
+
     app.run_server(debug=True)
 
